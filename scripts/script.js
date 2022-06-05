84462edea72294de62676a7b9de245cd5ee0f717
GiveMACode <vinicius_atanasio@hotmail.com>
2022-06-04 21:48:03 -0300

    2 completed

--- scripts/script.js
+++ scripts/script.js
@@ -3,7 +3,7 @@ function at1() {
     var num2 = parseFloat(document.getElementById("n2").value);
 
     if (num1 > num2) {
-       document.getElementById("result").innerHTML = num2 + "  e  " + num1;
+        document.getElementById("result").innerHTML = num2 + "  e  " + num1;
     }
     else {
         document.getElementById("result").innerHTML = num1 + "  e  " + num2;
@@ -28,10 +28,47 @@ function at2() {
 
     if (gender == 'feminino') {
         document.getElementById("resultado").innerHTML = arredondadoF
-    console.log(arredondadoF)
+        console.log(arredondadoF)
     }
-        else if (gender == 'masculino') {
-            document.getElementById("resultado").innerHTML = arredondadoM
-        }
+    else if (gender == 'masculino') {
+        document.getElementById("resultado").innerHTML = arredondadoM
+    }
 
+}
+
+function at3() {
+
+    var num1 = parseFloat(document.getElementById("n1").value);
+    var num2 = parseFloat(document.getElementById("n2").value);
+    var num3 = parseFloat(document.getElementById("n3").value);
+
+    var menornumero = num1;
+
+    if (num2 < menornumero) {
+        menornumero = num2;
     }
+    if (num3 < menornumero) {
+        menornumero = num3;
+    }
+
+    document.getElementById("resultado").innerHTML = menornumero;
+
+
+}
+
+function at4() {
+
+    var num1 = parseFloat(document.getElementById("n1").value);
+    var total;
+
+    if (num1 >= 0) {
+        var aux = num1 % 2 === 0 ? 'par' : 'impar'
+
+        document.getElementById("resultado").innerHTML = aux;
+    }
+    else {
+        document.getElementById("resultado").innerHTML = Math.abs(num1) ;
+    }
+
+    
+}
